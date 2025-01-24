84fb3941446e4d9eb8e4a4ad62a70a4f46d08a12
Michael <michael.vardanyan@web.de>
2025-01-24 15:06:03 +0100

    allow indiviual lookup of project id

--- src/main/java/dev/bymichael/portfoliobackend/controller/ProjectController.java
+++ src/main/java/dev/bymichael/portfoliobackend/controller/ProjectController.java
@@ -3,11 +3,10 @@ package dev.bymichael.portfoliobackend.controller;
 import dev.bymichael.portfoliobackend.model.Project;
 import dev.bymichael.portfoliobackend.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +18,9 @@ public class ProjectController {
     @GetMapping("/projects")
     public List<Project> getProjects() {
         return projectRepository.findAll();
+    }
+    @GetMapping("/project/{projectId}")
+    public Optional<Project> getProjectById(@PathVariable String projectId) {
+        return projectRepository.findById(projectId);
     }
 }
